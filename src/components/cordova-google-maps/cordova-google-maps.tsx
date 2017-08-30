d1dd5d28879b4d4812378b2b90b01a803616c1c3
Ibby Hadeed <ibby93@gmail.com>
2017-08-30 02:55:33 -0400

    native works

--- src/components/cordova-google-maps/cordova-google-maps.tsx
+++ src/components/cordova-google-maps/cordova-google-maps.tsx
@@ -1,12 +1,25 @@
-import { Component } from '@stencil/core';
+import { Component, Element } from '@stencil/core';
+
+declare var plugin: any;
 
 @Component({
-    tag: 'cordova-google-maps'
+    tag: 'cordova-google-maps',
+    styles: 'cordova-google-maps .map { width: 500px; height: 500px; max-width: 100%; max-height: 100%; display: block; }'
 })
 export class CordovaGoogleMaps {
+
+    @Element() element: HTMLElement;
+
+    mapElement: HTMLElement;
+
+    map: any;
+
+    componentDidLoad() {
+        this.mapElement = this.element.querySelector('div.map') as HTMLElement;
+        this.map = plugin.google.maps.Map.getMap(this.mapElement);
+    }
+
     render() {
-        return (
-            <div></div>
-        );
+        return <div class="map"></div>;
     }
 }
